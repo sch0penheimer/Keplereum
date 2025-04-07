073050dae6939562f252f2aad242e6af071986e2
Lenovo <machine.bidi66@gmail.com>
2025-04-07 07:02:29 +0100

    - Successfuly implemented: Satellite Real-Time tracking + Real-time Satellite Telemetry Fetching.

---
+++ FrontEnd/src/utils/constants.ts
@@ -0,0 +1,5 @@
+  //* Constants *//
+  export const earthRadius = 6371;
+  export const earthRadiusSceneUnits = 4;
+  export const scaleFactor = earthRadiusSceneUnits / earthRadius;
+  export const satelliteModelPath = 'main-sat.glb';

ce8735d5080d9c6674d92269f013f847c34a4c2e
Mharfe23 <fengiro.arafa@gmail.com>
2025-05-26 03:51:22 +0100

    -Added tests + allowed all cors origins

--- Backend_microservice/apiGateway/src/main/java/com/example/apiGateway/apiGateway/config/SecurityConfig.java
+++ Backend_microservice/apiGateway/src/main/java/com/example/apiGateway/apiGateway/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -33,7 +34,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001"));
+        configuration.setAllowedOriginPatterns(Collections.singletonList("*"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Accept"));
         configuration.setAllowCredentials(true);
